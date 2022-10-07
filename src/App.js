6197616b8b0321a54a8dfc02b8191383437a47c3
Julia Komarnitska <juliaowlghost@gmail.com>
2022-10-07 16:41:13 +0300

    homework

--- src/App.js
+++ src/App.js
@@ -1,67 +1,29 @@
-import cat from './cat.jpg';
 import './App.css';
 import React from 'react';
 
-const style = {
-  block: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginBottom: '50px',
-    border: '2px solid white',
-  },
-  element: {
-    paddingBottom: '20px',
-  }
-}
+import { Greetings } from './components/Greetings';
+import { Card} from './components/Cards';
 
-function Greetings(props) {
-  return (
-    <div style={style.block}>
-      <h1>Hello, {props.name}!</h1>
-      <img src={cat} className="cat" alt="cat" />
-    </div>
-  )
-}
-
-class Info extends React.Component {
-  render() {
-    return ( 
-      <div className='card' style={style.block}>
-        <img className="img" style={style.element}
-          src={this.props.src}
-          alt={this.props.name}
-        />
-        <span className='img-name' style={style.element}>
-          {this.props.name}
-        </span>
-        <span className='date' style={style.element}>
-          {new Date().toLocaleDateString()}
-        </span>
-      </div>
-    )
-  }
-}
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Greetings name = 'World'></Greetings>
-        <Info 
-          src = 'https://www.petrebels.com/wp-content/uploads/2021/05/Maine-Coon-1-S_0.jpg' 
-          name = 'MaineCoon'>
-        </Info>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Link to='/'> Greetings </Link>
+          <Link to='cards'> Cards </Link>
+          <Routes>
+            <Route path="/" element={<Greetings name='World' />} />
+            <Route path="cards" element={<Card/>} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
